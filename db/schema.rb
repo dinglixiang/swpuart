e308584378b9acf1b06762ef6b73068d6410d902
wangyibin <775461028@qq.com>
2013-04-11 08:11:54 +0800

    gai

--- db/schema.rb
+++ db/schema.rb
@@ -113,12 +113,6 @@ ActiveRecord::Schema.define(:version => 20130409073207) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-  create_table "vedios", :force => true do |t|
-    t.string   "vname"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "videos", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
