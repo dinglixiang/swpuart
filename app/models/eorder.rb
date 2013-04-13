afed83f0ed8fabce2e02f6024cdb8e875f355eda
丁历祥 <dlx_swpu@qq.com>
2013-04-13 23:09:52 +0800

    some validates

--- app/models/eorder.rb
+++ app/models/eorder.rb
@@ -1,5 +1,10 @@
 class Eorder < ActiveRecord::Base
   attr_accessible :ename,:eapplicant, :eartopinion, :ekeeper, :ereturntime, :esituation, :etel, :eusereason, :eusetime
+  validates :ename, :presence => true
+  validates :eapplicant, :presence => true
+  validates :etel, :presence => true
+  validates :eusetime, :presence => true
+  validates :eusereason, :presence => true
 
   has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,7 +1,11 @@
 class Equipment < ActiveRecord::Base
   attr_accessible :elist, :ename, :eprice, :eremain, :etype,:epicture
  
+  validates :ename, :presence => true
+  validates :eremain, :presence => true
+
   mount_uploader :epicture, AvatarUploader
 
   has_many :eorders, :dependent => :destroy
+
 end
