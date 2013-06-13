2447f6e5f270ce137e61194dfffd6f05376abbe8
丁历祥 <dlx_swpu@qq.com>
2013-06-14 04:42:12 +0800

    index show

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,8 @@ class StaticPagesController < ApplicationController
 
   def home
   	 @notices = Notice.limit(3).order('created_at desc')
-  	 @eorders = Eorder.where('eartopinion = ?',"同意").limit(8).order('created_at desc')
-  	 @sorders = Sorder.where('sopinion = ?',"同意").limit(8).order('created_at desc')
+  	 @eorders = Eorder.where('eartopinion = ?',"同意").limit(7).order('created_at desc')
+  	 @sorders = Sorder.where('sopinion = ?',"同意").limit(7).order('created_at desc')
   end
 
   def rules
